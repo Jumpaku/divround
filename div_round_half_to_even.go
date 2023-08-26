e634c63b8ae62ff3a308ce06b15e6fd45b5eed6d
Jumpaku <Jumpaku@users.noreply.github.com>
2023-08-26 18:18:28 +0000

    feat: add DivRoundHalfToEven

---
+++ div_round_half_to_even.go
@@ -0,0 +1,14 @@
+package div
+
+// DivRoundHalfToEven computes division followed by rounding for two integers accurately.
+// This function panics if b == 0.
+func DivRoundHalfToEven[T SignedInteger](a, b T) T {
+	if b == 0 {
+		panic(`cannot divide by 0`)
+	}
+	up := DivRoundHalfUp[T](a, b)
+	if (up & 1) != 1 {
+		return up
+	}
+	return DivRoundHalfDown[T](a, b)
+}
